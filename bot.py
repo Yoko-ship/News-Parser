96df270397b0cc3f3125dec44612be67ab80f50c
User <yuko.brandon13@gmail.com>
2025-09-01 15:46:34 +0500

    fastapi

--- bot.py
+++ bot.py
@@ -1,33 +1,41 @@
 from aiogram import Bot,Dispatcher
 from dotenv import load_dotenv
 from os import getenv
-import asyncio
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from db import get_from_table
-from flask import Flask
-
-app = Flask(__name__)
+from fastapi import FastAPI
+from aiogram.types import Update
 
 load_dotenv()
 token = getenv('token')
-dp = Dispatcher()
+app = FastAPI()
+bot = Bot(token,default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
+
+dp = Dispatcher()
 channel_id = getenv("CHANNEL_ID")
 
-async def main():
-    bot = Bot(token,default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+
+async def run_bot_task():
     await get_from_table(bot,parse=ParseMode.HTML,channel_id=channel_id)
     await bot.session.close()
 
-
-@app.route("/run-task")
-def run_task():
-    asyncio.run(main())
-    return "Task executed"
+@app.get("/")
+def home():
+    return {"status":"Сервер работает успешно"}
 
 
-app.run(host="0.0.0.0",port=5000)
+@app.get("/run-task")
+async def run_task():
+    try:
+        await run_bot_task()
+        return {"status":"Task executed"}
+    except Exception as e:
+        return {"status":e}
+
+
 
 
 
