e7d2d7d826599ab0e1118ecdcd33f29049cf1c1c
User <yuko.brandon13@gmail.com>
2025-09-01 15:32:34 +0500

    flask

--- bot.py
+++ bot.py
@@ -5,6 +5,9 @@ import asyncio
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from db import get_from_table
+from flask import Flask
+
+app = Flask(__name__)
 
 load_dotenv()
 token = getenv('token')
@@ -12,11 +15,19 @@ dp = Dispatcher()
 
 channel_id = getenv("CHANNEL_ID")
 
-
 async def main():
     bot = Bot(token,default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     await get_from_table(bot,parse=ParseMode.HTML,channel_id=channel_id)
     await bot.session.close()
 
 
-asyncio.run(main())
+@app.route("/run-task")
+def run_task():
+    asyncio.run(main())
+    return "Task executed"
+
+
+app.run(host="0.0.0.0",port=5000)
+
+
+
